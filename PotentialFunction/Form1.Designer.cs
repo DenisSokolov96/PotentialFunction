71b246ec079869332679f316ca358ce6359123d4
DenisSokolov96 <dens111296@gmail.com>
2020-11-24 19:49:52 +0300

    Добавьте файлы проекта.

---
+++ PotentialFunction/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PotentialFunction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.обучитьНаФайлеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.распознатьНаФайлеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.обучитьНаФайлеToolStripMenuItem,
+            this.распознатьНаФайлеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // обучитьНаФайлеToolStripMenuItem
+            // 
+            this.обучитьНаФайлеToolStripMenuItem.Name = "обучитьНаФайлеToolStripMenuItem";
+            this.обучитьНаФайлеToolStripMenuItem.Size = new System.Drawing.Size(119, 20);
+            this.обучитьНаФайлеToolStripMenuItem.Text = "Обучить на файле...";
+            this.обучитьНаФайлеToolStripMenuItem.Click += new System.EventHandler(this.обучитьНаФайлеToolStripMenuItem_Click);
+            // 
+            // распознатьНаФайлеToolStripMenuItem
+            // 
+            this.распознатьНаФайлеToolStripMenuItem.Name = "распознатьНаФайлеToolStripMenuItem";
+            this.распознатьНаФайлеToolStripMenuItem.Size = new System.Drawing.Size(138, 20);
+            this.распознатьНаФайлеToolStripMenuItem.Text = "Распознать на файле...";
+            this.распознатьНаФайлеToolStripMenuItem.Click += new System.EventHandler(this.распознатьНаФайлеToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 49);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(282, 389);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Инфо:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Классификатор по методу потенциальных функций";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem обучитьНаФайлеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem распознатьНаФайлеToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PotentialFunction/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PotentialFunction
+{
+    public partial class Form1 : Form
+    {
+        //список входных данных
+        private List<int[]> listVectos = new List<int[]>();
+        //список тестовых данных
+        private List<int[]> listTestVectos = new List<int[]>();
+
+        /*##############################################################################*/
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void обучитьНаФайлеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listVectos = redFile();
+        }
+                
+        private void распознатьНаФайлеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listTestVectos = redFile();
+        }
+
+        /*##############################################################################*/
+
+        private List<int[]> redFile()
+        {
+            List<int[]> list = new List<int[]>();
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "D:\\Универ\\2й курс магистратура\\Системы распознавания\\Классификатор по методу потенциальных функций\\PotentialFunction\\PotentialFunction\\datas";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog.FileName, Encoding.Default))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            richTextBox1.Text += line.ToString() + "\n";
+                            list.Add(parsToInt(line));
+                        }
+                    }
+                }
+            }
+            richTextBox1.Text += "Файл загружен.\n";
+            return list;
+        }
+
+        private int[] parsToInt(string line)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] != '\t')
+                    list.Add(Convert.ToInt32(line[i].ToString()));
+            }
+
+            return list.ToArray();
+        }
+
+    }
+}
