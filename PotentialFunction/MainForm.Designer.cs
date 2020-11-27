ffafabb6922145b4417cd664f400c72bb459f7be
DenisSokolov96 <dens111296@gmail.com>
2020-11-27 22:25:29 +0300

    детерминированный(для двух)

--- PotentialFunction/MainForm.Designer.cs
+++ PotentialFunction/MainForm.Designer.cs
@@ -33,17 +33,25 @@
             this.распознатьНаФайлеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.обучитьНаФайлеToolStripMenuItem,
             this.распознатьНаФайлеToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(449, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(768, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -65,7 +73,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(12, 49);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(426, 389);
+            this.richTextBox1.Size = new System.Drawing.Size(564, 389);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
@@ -78,11 +86,74 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Инфо:";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(592, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(592, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ввод для проверки:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(592, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Распознать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(594, 175);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.trackBar1.Maximum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(160, 45);
+            this.trackBar1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(581, 160);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Детерминированный";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(669, 194);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Стохастический";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 450);
+            this.ClientSize = new System.Drawing.Size(768, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
@@ -91,6 +162,7 @@
             this.Text = "Классификатор по методу потенциальных функций";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,6 +175,12 @@
         private System.Windows.Forms.ToolStripMenuItem распознатьНаФайлеToolStripMenuItem;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- PotentialFunction/MainForm.cs
+++ PotentialFunction/MainForm.cs
@@ -8,21 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-
+//MK2
+//стох
+//3 3 - 2
+//2 2 - 1
+//9 8 -2
+//-5 -4 - 2
+//детер
+//-5 -4 - 1
+//1,5 1,5 - 1 
+// 9 8 - 2
+// 0 0 - 1
+// -1 4 - 1
 namespace PotentialFunction
 {
     public partial class MainForm : Form
     {
         //список входных данных
-        private List<int[]> listVectors = new List<int[]>();
+        public List<double[]> listVectors = new List<double[]>();
         //список тестовых данных
-        private List<int[]> listTestVectos = new List<int[]>();
+        public List<double[]> listTestVectors = new List<double[]>();
         //список куммулятивных потенциалов
-        private List<double> listK = new List<double>();
+        public List<double> listK = new List<double>();
         //список знаков
-        private List<int> listZn = new List<int>();
-        //Список индексов элементов для расчета экспонент
-        private List<int> listExp = new List<int>();
+        public List<int> listZn = new List<int>();
+        //список формул
+        public List<double> listFormuls = new List<double>();
 
         /*##############################################################################*/
 
@@ -33,107 +44,175 @@ namespace PotentialFunction
 
         private void обучитьНаФайлеToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            listVectors = redFile();
-            learning();
+            listVectors.Clear();
+            listVectors = readFile();
+            listZn.Clear();
+            listFormuls.Clear();
+            //детерминированный
+            if (trackBar1.Value == 0)
+            {
+                learning();
+            }
+            //стохастический
+            else
+            {
+
+            }
+
             richTextBox1.Text += "Обучение завершено.\n";
         }
                 
         private void распознатьНаФайлеToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            listTestVectos = redFile();
-            recognition();
-            richTextBox1.Text += "Распазнавание завершено.\n";
+            listTestVectors.Clear();
+            listTestVectors = readFile();
+            //детерминированный
+            if (trackBar1.Value == 0)
+            {               
+                recognitionFile();                
+            }
+            //стохастический
+            else
+            {
+
+            }
+
+            richTextBox1.Text += "Распознавание завершено.\n";
         }
 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listTestVectors.Clear();
+            //детерминированный
+            if (trackBar1.Value == 0)
+            {                
+                listTestVectors.Add(parsToInt("0 " + textBox1.Text.ToString()));
+                if (listTestVectors[0].Length - 1 < 3)
+                    recognitionFile();
+                else recognitionFileV3();
+            }
+            //стохастический
+            else
+            {
+
+            }
+            richTextBox1.Text += "Распознавание завершено.\n";
+        }
+
+        /*                                      2                                       */
         /*##############################################################################*/
 
         private void learning()
-        {           
+        {
             int check = 0;
-            int indnClass = 0;            
+            int epoch = 1;
 
             //Первый шаг
-            if (listVectors[indnClass][0] == 1)
-            {
+            if (listVectors[0][0] == 1)
                 listZn.Add(1);
-                listExp.Add(0);
-            }
-            else { listZn.Add(-1); }
+            else listZn.Add(-1);
 
+            for (int i = 1; i < listVectors[0].Length; i++)
+                listFormuls.Add(listVectors[0][i]);
+
+            int iVector = 1;
             //Следующие шаги
-            while (check < listVectors.Count()-1)
+            while ((check < listVectors.Count()) && (listVectors.Count() > 1))
             {
-                double K = 0;
-                for (int i = 0; i < listZn.Count; i++)
+                double K = funcK(iVector, listVectors);
+
+                if ((K > 0) && (listVectors[iVector][0] == 1.0))
                 {
-                    for (int j = 0; j < listVectors[0].Length - 1; j++)
-                    {
-                        K += Math.Exp(-Math.Pow(listVectors[indnClass][j + 1] - listVectors[Convert.ToInt32(listExp[i])][j + 1], 2)
-                                      ) * Convert.ToDouble(listZn[i]);
-                    }
-
+                    check++;
                 }
-
-                if (((K > 0) && (listVectors[indnClass][0] == 1)) || ((K < 0) && (listVectors[indnClass][0] == 2)))
-                { check++; }
-                else if ((K <= 0) && (listVectors[indnClass][0] == 1))
+                if ((K < 0) && (listVectors[iVector][0] == 2.0))
+                {
+                    check++;
+                }
+                if ((K <= 0) && (listVectors[iVector][0] == 1.0))
                 {
                     check = 0;
                     listZn.Add(1);
-                    listExp.Add(indnClass);
+                    for (int i = 1; i < listVectors[iVector].Length; i++)
+                        listFormuls.Add(listVectors[iVector][i]);
                 }
-                else if ((K >= 0) && (listVectors[indnClass][0] == 2))
+                if ((K >= 0) && (listVectors[iVector][0] == 2.0))
                 {
                     check = 0;
                     listZn.Add(-1);
-                    listExp.Add(indnClass);
+                    for (int i = 1; i < listVectors[iVector].Length; i++)
+                        listFormuls.Add(listVectors[iVector][i]);
                 }
-                if (indnClass == listVectors.Count - 1)
-                {
-                    indnClass = 0;
-                }
-                else indnClass++;
-                
-                listK.Add(K);               
-            }
-            richTextBox1.Text += "Кол-во итераций обучения: " + listK.Count.ToString() + "\n";
-            for (int i = 0; i < listExp.Count; i++)
-            {
-                richTextBox1.Text += "**************************************\n";
-                richTextBox1.Text += listExp[i].ToString() + "\n";
-                richTextBox1.Text += "**************************************\n";
-            }
 
+                //проверка, дошли ли мы до конца
+                if (iVector == listVectors.Count - 1)
+                {
+                    iVector = 0;
+                }
+                else iVector++;
+
+                listK.Add(K);// можно убрать                
+                epoch++;
+            }
+            richTextBox1.Text += "Кол-во итераций обучения: " + epoch.ToString() + "\n";
         }
 
-        private void recognition()
+        private double funcK(int iVector, List<double[]> list)
         {
-            double k = 0.0;
-            double ksum = 0.0;
-            for (int j = 0; j < listTestVectos.Count; j++)
+            double k = 0;
+            for (int iExp = 0; iExp < listZn.Count(); iExp++)
             {
-                k = 0.0;
-                ksum = 0.0;
-                for (int i = 0; i < listZn.Count; i++)
+                double exp = 0;
+                for (int j = 1; j < list[iVector].Length; j++)
                 {
-                    k = 0.0;
-                    for (int g = 0; g < listTestVectos[0].Length-1; g++)
-                    {
-                        k += Math.Exp(-Math.Pow(listTestVectos[j][g] - listVectors[Convert.ToInt32(listExp[i])][g + 1], 2));
-                    }
-                    ksum += k * Convert.ToDouble(listZn[i]);
+                    int a = iExp * (list[iVector].Length - 1);
+                    exp += Math.Pow(list[iVector][j] - listFormuls[a + j - 1], 2);
                 }
-                if (ksum < 0) richTextBox1.Text += "2\n";
-                else richTextBox1.Text += "1\n";
-                
+                k += Math.Exp(-exp) * listZn[iExp];
+            }
+            return k;
+        }
+
+        private void recognitionFile()
+        {
+            for (int iVector = 0; iVector < listTestVectors.Count; iVector++)
+            {
+                double k = funcK(iVector, listTestVectors);
+
+                if (k < 0) richTextBox1.Text += "2:   ";
+                else richTextBox1.Text += "1:  ";
+
+                for (int i = 1; i < listTestVectors[iVector].Length; i++)
+                    richTextBox1.Text += listTestVectors[iVector][i].ToString() + "   ";
+                richTextBox1.Text += "\n";
+            }
+        }
+
+        /*                        3 и больше                                            */
+        /*##############################################################################*/
+
+        private void recognitionFileV3()
+        {
+            List<double> listK = new List<double>();
+            for (int iVector = 0; iVector < listTestVectors.Count; iVector++)
+            {
+                double k = funcK(iVector, listTestVectors);
+
+                if (k < 0) richTextBox1.Text += "2:   ";
+                else richTextBox1.Text += "1:  ";
+
+                for (int i = 1; i < listTestVectors[iVector].Length; i++)
+                    richTextBox1.Text += listTestVectors[iVector][i].ToString() + "   ";
+                richTextBox1.Text += "\n";
             }
         }
 
         /*##############################################################################*/
 
-        private List<int[]> redFile()
+        private List<double[]> readFile()
         {
-            List<int[]> list = new List<int[]>();
+            List<double[]> list = new List<double[]>();
             var filePath = string.Empty;
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
@@ -159,17 +238,14 @@ namespace PotentialFunction
             return list;
         }
 
-        private int[] parsToInt(string line)
+        private double[] parsToInt(string line)
         {
-            List<int> list = new List<int>();
-            for (int i = 0; i < line.Length; i++)
-            {
-                if (line[i] != '\t')
-                    list.Add(Convert.ToInt32(line[i].ToString()));
-            }
-
+            List<double> list = new List<double>();
+            string[] mas = line.Split(' ', '\t');
+            for (int i = 0; i < mas.Length; i++)            
+                list.Add(Convert.ToDouble(mas[i].ToString()));
+              
             return list.ToArray();
         }
-
     }
 }
